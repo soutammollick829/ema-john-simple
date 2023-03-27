c61a84052976a4210cf44359c5edfb8ce3459708
soutammolick829 <soutammollick829@gmail.com>
2023-03-28 03:55:43 +0600

    order summery show data

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import { faRankingStar, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 const Product = (props) => {
-  console.log(props)
+  // console.log(props)
     const {img,name,price,ratings,seller} = props.product;
     const handelAddToCart = props.handelAddToCart;
-
 
     return (
         <>
@@ -16,9 +15,27 @@ const Product = (props) => {
     <h2 className="text-xl font-semibold lowercase">{name}</h2>
     <p className='text-xl mt-5'>Price : ${price}</p>
     <p className='mt-7'>Manufacturer : {seller}</p>
+    <div className='flex'>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-orange-400">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+</svg>
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-orange-400">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+</svg>
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-orange-400">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+</svg>
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-orange-400">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+</svg>
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-slate-400">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+</svg>
+    </div>
+
     <p>Rating : {ratings}</p>
   </div>
-  <button onClick={() => handelAddToCart(props.Product)} className="bg-orange-300 w-full h-14 mt-4 mb-0 hover:bg-orange-400">Add to Cart
+  <button onClick={() => handelAddToCart(props.product)} className="bg-orange-300 w-full h-14 mt-4 mb-0 hover:bg-orange-400">Add to Cart
   <FontAwesomeIcon icon={faShoppingCart} />
 </button>
 </div>
--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -4,28 +4,37 @@ import './Shop.css'
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [cart , setCart] = useState([]);
     useEffect(()=>{
         fetch(`products.json`)
         .then(res => res.json())
         .then(data => setProducts(data))
     },[]);
 
-
-    const handelAddToCart = (products) =>{
-        console.log(products)
+    const handelAddToCart = (product) =>{
+        // console.log(product)
+        const newCart = [...cart, product];
+        setCart(newCart);
       }
 
     return (
         <div className='shop-container'>
             <div className="products-container grid grid-cols-3 gap-10 mx-10 mt-10">
-                {
-                    products.map(product => <Product key={product.id} product={product} 
-                    handelAddToCart={handelAddToCart}
-                    ></Product>)
-                }  
+    {
+        products.map(product => <Product key={product.id} product={product} 
+        handelAddToCart={handelAddToCart}
+        ></Product>)
+    }  
             </div>
             <div className="text-center text-2xl mt-3 bg-orange-200">
-            Order Summary
+            <h3 className='text-xl font-bold mt-4'>Order Summary</h3>
+            <div className='text-lg p-5 space-y-5'>
+            <p className='text-lg '>Selected Items: {cart.length} </p>
+            <p>Total Price:</p>
+            <p>Total Shipping Charge:</p>
+            <p>Tax: </p>
+            <p className='text-lg mt-5 font-bold'>Grand Total:</p>
+            </div>
             </div>
         </div>
     );
